73d13ac29d72980dbf0a4ffabb1535192acaca26
Gabriel de Sousa Rolim <gabrielsouza@codata.pb.gov.br>
2018-04-04 11:31:13 -0300

    Finalizado aula 3

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux'
+import calendar from '../reducers';
+import { addRecipe, removeFromCalendar } from '../actions';
 
 class App extends Component {
   render() {
+    console.log('Props',this.props)
     return (
       <div className="App">
         <header className="App-header">
@@ -18,4 +22,34 @@ class App extends Component {
   }
 }
 
-export default App;
+
+
+function mapStateToProps (calendar) {
+  const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrder.map((day) => ({
+      day,
+      meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+        meals[meal] = calendar[day][meal]
+          ? calendar[day][meal]
+          : null
+
+        return meals
+      }, {})
+    })),
+  }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data)),
+  }
+
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
